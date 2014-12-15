8a6eb6476c3ccb5f2d63229ef53dc318e8595aed
Francis Galiegue <fgaliegue@gmail.com>
2014-12-15 11:14:28 +0100

    Create base files

---
+++ src/main/java/com/github/fge/filesystem/MoreFiles.java
@@ -0,0 +1,9 @@
+package com.github.fge.filesystem;
+
+public final class MoreFiles
+{
+    private MoreFiles()
+    {
+        throw new Error("nice try!");
+    }
+}
---
+++ src/main/java/com/github/fge/filesystem/MorePaths.java
@@ -0,0 +1,9 @@
+package com.github.fge.filesystem;
+
+public final class MorePaths
+{
+    private MorePaths()
+    {
+        throw new Error("nice try!");
+    }
+}
