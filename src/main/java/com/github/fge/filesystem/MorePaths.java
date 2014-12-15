adb3519399134ef465bbd1e56aacf0e32bc07f86
Francis Galiegue <fgaliegue@gmail.com>
2014-12-15 16:47:16 +0100

    Replace tabs with spaces

--- src/main/java/com/github/fge/filesystem/MorePaths.java
+++ src/main/java/com/github/fge/filesystem/MorePaths.java
@@ -9,22 +9,23 @@ public final class MorePaths
     {
         throw new Error("nice try!");
     }
-    
+
     /**
-     * 
      * @param path1
      * @param path2
      * @return
      */
-    public static Path resolve(Path path1, Path path2) {
-    	final FileSystem fs1 = path1.getFileSystem();
-		final FileSystem fs2 = path2.getFileSystem();
-		if(fs1 == fs2)
-    		return path1.resolve(path2);
-    	
-		if(fs1.provider() == fs2.provider())
-    		return path1.resolve(fs1.getPath(path2.toString()));
-		
-		return null;//TODO: Needs to do something
+    public static Path resolve(Path path1, Path path2)
+    {
+        final FileSystem fs1 = path1.getFileSystem();
+        final FileSystem fs2 = path2.getFileSystem();
+
+        if (fs1 == fs2)
+            return path1.resolve(path2);
+
+        if (fs1.provider() == fs2.provider())
+            return path1.resolve(fs1.getPath(path2.toString()));
+
+        return null; //TODO
     }
 }
--- src/test/java/com/github/fge/filesystem/MorePathsTest.java
+++ src/test/java/com/github/fge/filesystem/MorePathsTest.java
@@ -1,64 +1,64 @@
 package com.github.fge.filesystem;
 
-import static org.mockito.Matchers.same;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 import java.nio.file.spi.FileSystemProvider;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public final class MorePathsTest
 {
-	private Path path1;
-	private Path path2;
-	private FileSystem fs1;
-	private FileSystem fs2;
-	private FileSystemProvider provider;
-	private final Path path3 = mock(Path.class); 
+    private Path path1;
+    private Path path2;
+    private FileSystem fs1;
+    private FileSystem fs2;
+    private FileSystemProvider provider;
+    private final Path path3 = mock(Path.class);
 
-	@BeforeMethod
-	public void initMocks()
-	{
-		path1 = mock(Path.class);
-		path2 = mock(Path.class);
-		fs1 = mock(FileSystem.class);
-		fs2 = mock(FileSystem.class);
-		provider = mock(FileSystemProvider.class);
-	}
+    @BeforeMethod
+    public void initMocks()
+    {
+        path1 = mock(Path.class);
+        path2 = mock(Path.class);
+        fs1 = mock(FileSystem.class);
+        fs2 = mock(FileSystem.class);
+        provider = mock(FileSystemProvider.class);
+    }
 
-	@Test
-	public void resolvingPathWithSameFileSystemCallsFirstPathResolve()
-	{
-		when(path1.getFileSystem()).thenReturn(fs1);
-		when(path2.getFileSystem()).thenReturn(fs1);
+    @Test
+    public void resolvingPathWithSameFileSystemCallsFirstPathResolve()
+    {
+        when(path1.getFileSystem()).thenReturn(fs1);
+        when(path2.getFileSystem()).thenReturn(fs1);
 
-		MorePaths.resolve(path1, path2);
+        MorePaths.resolve(path1, path2);
 
-		verify(path1).resolve(same(path2));
-	}
-	
-	@Test
-	public void resolvingPathWithSameFileSystemProvider()
-	{
-		
-		final String nameElement = "test";
-		
-		when(path1.getFileSystem()).thenReturn(fs1);
-		when(path2.getFileSystem()).thenReturn(fs2);
-		
-		when(fs1.provider()).thenReturn(provider);
-		when(fs2.provider()).thenReturn(provider);
+        verify(path1).resolve(same(path2));
+    }
 
-		when(path2.toString()).thenReturn(nameElement);
-		when(fs1.getPath(same(nameElement))).thenReturn(path3); 
-		
-		MorePaths.resolve(path1, path2);
-		
-		verify(path1).resolve(same(path3));
-	}
+    @Test
+    public void resolvingPathWithSameFileSystemProvider()
+    {
+
+        final String nameElement = "test";
+
+        when(path1.getFileSystem()).thenReturn(fs1);
+        when(path2.getFileSystem()).thenReturn(fs2);
+
+        when(fs1.provider()).thenReturn(provider);
+        when(fs2.provider()).thenReturn(provider);
+
+        when(path2.toString()).thenReturn(nameElement);
+        when(fs1.getPath(same(nameElement))).thenReturn(path3);
+
+        MorePaths.resolve(path1, path2);
+
+        verify(path1).resolve(same(path3));
+    }
 } 
