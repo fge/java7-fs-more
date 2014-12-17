b452f160d5d0beb803e18fc199df26933d021e61
Francis Galiegue <fgaliegue@gmail.com>
2014-12-18 00:05:37 +0100

    MorePaths: add javadoc; refactor .resolve()

--- src/main/java/com/github/fge/filesystem/MorePaths.java
+++ src/main/java/com/github/fge/filesystem/MorePaths.java
@@ -4,9 +4,19 @@ import com.github.fge.filesystem.exception.UnresolvablePathException;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.nio.file.FileSystem;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.ProviderMismatchException;
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Objects;
 
+/**
+ * {@link Path} utility methods
+ *
+ * <p>Unless otherwise noted, all methods in this class do not accept null
+ * arguments and will throw a {@link NullPointerException} if a null argument
+ * is passed to them.</p>
+ */
 @ParametersAreNonnullByDefault
 public final class MorePaths
 {
@@ -16,9 +26,59 @@ public final class MorePaths
     }
 
     /**
-     * @param path1
-     * @param path2
-     * @return
+     * Resolve a path against another path with a potentially different
+     * {@link FileSystem} or {@link FileSystemProvider}
+     *
+     * <p>{@link Path#resolve(Path)} will refuse to operate if its argument is
+     * issued from a different provider (with a {@link
+     * ProviderMismatchException}); moreover, if the argument is issued from the
+     * same provider but is on a different filesystem, the result of the
+     * resolution may be on the argument's filesystem, not the caller's.</p>
+     *
+     * <p>This method will attempt to resolve the second path against the first
+     * so that the result is <em>always</em> associated to the filesystem (and
+     * therefore provider) of the first argument. For the resolution to operate,
+     * the following conditions must be met for {@code path2}:</p>
+     *
+     * <ul>
+     *     <li>if it is not absolute, it must not have a root;</li>
+     *     <li>if it is absolute, it must have a root, and the string
+     *     representation of this root must match a string representation of one
+     *     possible root of the first path's filesystem.</li>
+     * </ul>
+     *
+     * <p>If the conditions above are not satisfied, this method throws an
+     * {@link UnresolvablePathException} (unchecked).</p>
+     *
+     * <p>If both paths are issued from the same filesystem, this method will
+     * delegate to {@code path1}'s {@code .resolve()}; if they are from
+     * different filesystems but share the same provider, this method returns:
+     * </p>
+     *
+     * <pre>
+     *     path1.resolve(path1.getFileSystem().getPath(path2.toString()))
+     * </pre>
+     *
+     * <p>This means that for instance it is possible to resolve a Unix path
+     * against a Windows path, or the reverse, as long as the second path is
+     * not absolute (the root paths of both filesystems are incompatible):</p>
+     *
+     * <ul>
+     *     <li>resolving {@code foo/bar/baz} against {@code c:} will return
+     *     {@code c:\foo\bar\baz};</li>
+     *     <li>resolving {@code baz\quux} against {@code /foo/bar} will return
+     *     {@code /foo/bar/baz/quux}.</li>
+     * </ul>
+     *
+     * @param path1 the first path
+     * @param path2 the second path
+     * @return the resolved path
+     * @throws UnresolvablePathException see description
+     * @throws InvalidPathException {@code path2} is from a different provider,
+     * and one of its name elements is invalid according to {@code path1}'s
+     * filesystem
+     *
+     * @see FileSystem#getPath(String, String...)
      */
     @SuppressWarnings("ObjectEquality")
     public static Path resolve(final Path path1, final Path path2)
@@ -35,51 +95,54 @@ public final class MorePaths
         if (fs1.provider() == fs2.provider())
             return path1.resolve(fs1.getPath(path2.toString()));
 
-        if(!path2.isAbsolute())
-            return resolvePath2NotAbsolute(path1, path2);
+        final boolean isAbsolute = path2.isAbsolute();
+        final Path root2 = path2.getRoot();
 
-        if (path2.getRoot() == null)
-            throw new UnresolvablePathException("path to resolve is absolute "
-                + "but has no root");
+        final String errmsg = isAbsolute
+            ? "path to resolve is absolute but has no root"
+            : "path to resolve is not absolute but has a root";
 
-        final String p2Root = path2.getRoot().toString();
-        final Iterable<Path> roots = path1.getFileSystem().getRootDirectories();
+        // Always tricky to read an xor...
+        if (isAbsolute ^ root2 != null)
+            throw new UnresolvablePathException(errmsg);
 
-        boolean matchingRoot = false;
-        for(final Path root: roots)
-            if (root.toString().equals(p2Root))
-                matchingRoot = true;
+        Path ret;
 
-        if (!matchingRoot)
-            throw new UnresolvablePathException("root of path to resolve "
-                + "is incompatible with source path");
+        if (isAbsolute) {
+            /*
+             * Check if the root of path2 is compatible with path1
+             */
+            final String s = root2.toString();
 
-        Path result = fs1.getPath(p2Root);
+            boolean foundRoot = false;
+
+            for (final Path root1: fs1.getRootDirectories())
+                if (root1.toString().equals(s))
+                    foundRoot = true;
+
+            if (!foundRoot)
+                throw new UnresolvablePathException("root of path to resolve "
+                    + "is incompatible with source path");
+
+            ret = fs1.getPath(s);
+        } else {
+            /*
+             * Since the empty path is defined as having one empty name
+             * component, which is rather awkward, we don't want to take the
+             * risk of triggering bugs in FileSystem#getPath(); instead, check
+             * that the string representation of path2 is empty, and if it is,
+             * just return path1.
+             */
+            if (path2.toString().isEmpty())
+                return path1;
+
+            ret = path1;
+        }
+
         for (final Path element: path2)
-            result = result.resolve(element.toString());
+            ret = ret.resolve(element.toString());
 
-        return result;
-
-    }
-
-    private static Path resolvePath2NotAbsolute(final Path path1,
-        final Path path2)
-    {
-
-        if(path2.toString().isEmpty())
-            return path1;
-
-        if (path2.getRoot() != null)
-            throw new UnresolvablePathException("path to resolve is not absolute but has a root");
-
-        Path result = path1;
-
-        //append all name elements of path2 with path1 because
-        // path2 is not absolute, not empty & has no root.
-        for (final Path element: path2)
-            result = result.resolve(element.toString());
-
-        return result;
+        return ret;
     }
 
 }
