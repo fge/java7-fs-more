9e2640c63712dfd61579f15e3dabddb60ba6557b
Francis Galiegue <fgaliegue@gmail.com>
2014-12-21 12:15:21 +0100

    InvalidIntModeException: create constructors

--- src/main/java/com/github/fge/filesystem/exceptions/InvalidIntModeException.java
+++ src/main/java/com/github/fge/filesystem/exceptions/InvalidIntModeException.java
@@ -3,4 +3,22 @@ package com.github.fge.filesystem.exceptions;
 public final class InvalidIntModeException
     extends IllegalArgumentException
 {
+    public InvalidIntModeException()
+    {
+    }
+
+    public InvalidIntModeException(final String s)
+    {
+        super(s);
+    }
+
+    public InvalidIntModeException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public InvalidIntModeException(final Throwable cause)
+    {
+        super(cause);
+    }
 }
