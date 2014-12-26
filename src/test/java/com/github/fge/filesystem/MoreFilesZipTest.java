4bda96a1c7765ae3e552a25a5ca960da74e63b39
Shakeel Shahzad <quaidian5@yahoo.com>
2014-12-27 01:18:42 +0500

    openZip: IsDirectoryException
    If path is a directory, throw IsDirectoryException

--- src/test/java/com/github/fge/filesystem/MoreFilesZipTest.java
+++ src/test/java/com/github/fge/filesystem/MoreFilesZipTest.java
@@ -1,24 +1,32 @@
 package com.github.fge.filesystem;
 
 
+import com.github.fge.filesystem.exceptions.IsDirectoryException;
 import com.github.marschall.memoryfilesystem.MemoryFileSystemBuilder;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
-import static com.github.fge.filesystem.helpers.CustomAssertions.shouldHaveThrown;
+import static com.github.fge.filesystem.helpers.CustomAssertions
+    .shouldHaveThrown;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public final class MoreFilesZipTest
 {
     private FileSystem fs;
     private Path path;
     private Path symbolicLink;
+    private Path directory;
 
     @BeforeClass
     public void initFs()
@@ -27,8 +35,10 @@ public final class MoreFilesZipTest
         fs = MemoryFileSystemBuilder.newLinux().build("MoreFilesZipTest");
         path = fs.getPath("existing.zip");
         symbolicLink = fs.getPath("symbolic.zip");
+        directory = fs.getPath("dir");
         Files.createFile(path);
-        Files.createSymbolicLink(symbolicLink,path);
+        Files.createSymbolicLink(symbolicLink, path);
+        Files.createDirectory(directory);
     }
 
     @Test
@@ -39,9 +49,9 @@ public final class MoreFilesZipTest
             MoreFiles.openZip(mock(Path.class), mock(OpenOption.class));
             shouldHaveThrown(UnsupportedOperationException.class);
         } catch (UnsupportedOperationException e) {
-            assertThat(e)
-                .isExactlyInstanceOf(UnsupportedOperationException.class)
-                .hasMessage("option is not supported");
+            assertThat(e).isExactlyInstanceOf(
+                UnsupportedOperationException.class).hasMessage(
+                "option is not supported");
         }
     }
 
@@ -52,8 +62,7 @@ public final class MoreFilesZipTest
             MoreFiles.openZip(path, StandardOpenOption.CREATE_NEW);
             shouldHaveThrown(IOException.class);
         } catch (IOException e) {
-            assertThat(e)
-                .isExactlyInstanceOf(FileAlreadyExistsException.class)
+            assertThat(e).isExactlyInstanceOf(FileAlreadyExistsException.class)
                 .hasMessage(path.toString());
         }
 
@@ -66,9 +75,21 @@ public final class MoreFilesZipTest
             MoreFiles.openZip(symbolicLink, LinkOption.NOFOLLOW_LINKS);
             shouldHaveThrown(IOException.class);
         } catch (IOException e) {
-            assertThat(e)
-                    .isExactlyInstanceOf(IOException.class)
-                    .hasMessage("refusing to open a symbolic link as a zip file");
+            assertThat(e).isExactlyInstanceOf(IOException.class).hasMessage(
+                "refusing to open a symbolic link as a zip file");
+        }
+
+    }
+
+    @Test
+    public void pathIsADirectoryThrowsIsDirectoryException()
+    {
+        try {
+            MoreFiles.openZip(directory, LinkOption.NOFOLLOW_LINKS);
+            shouldHaveThrown(IsDirectoryException.class);
+        } catch (IOException e) {
+            assertThat(e).isExactlyInstanceOf(IsDirectoryException.class)
+                .hasMessage("refusing to open a directory as a zip file");
         }
 
     }
