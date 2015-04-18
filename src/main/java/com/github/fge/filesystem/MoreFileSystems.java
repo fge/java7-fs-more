3959680beaad190e368750cd24acd05a561703c4
Francis Galiegue <fgaliegue@gmail.com>
2015-04-18 14:01:43 +0200

    Implement MoreFileSystems

--- src/main/java/com/github/fge/filesystem/MoreFileSystems.java
+++ src/main/java/com/github/fge/filesystem/MoreFileSystems.java
@@ -1,11 +1,17 @@
 package com.github.fge.filesystem;
 
+import com.github.fge.filesystem.readonly.ReadOnlyFileSystem;
+
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.WillNotClose;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Map;
@@ -14,6 +20,17 @@ import java.util.Objects;
 /**
  * Utility class for {@link FileSystem}s
  *
+ * <p>This utility class offers methods to:</p>
+ *
+ * <ul>
+ *     <li>open zip files as file systems (therefore including jars, wars etc);
+ *     </li>
+ *     <li>wrap existing file systems in a read only wrapper.</li>
+ * </ul>
+ *
+ * <p>It is the user's responsibility to {@link FileSystem#close() close} the
+ * filesystems returned by these methods.</p>
+ *
  * <p>Unless otherwise noted, all methods in this class do not accept null
  * arguments and will throw a {@link NullPointerException} if a null argument
  * is passed to them.</p>
@@ -21,22 +38,77 @@ import java.util.Objects;
 @ParametersAreNonnullByDefault
 public final class MoreFileSystems
 {
-    private static final Map<String, ?> ZIP_ENV = Collections.emptyMap();
+    private static final Map<String, ?> ZIP_NOCREATE = Collections.emptyMap();
+    private static final Map<String, ?> ZIP_CREATE
+        = Collections.singletonMap("create", "true");
 
     private MoreFileSystems ()
     {
         throw new Error("nice try!");
     }
 
+    /**
+     * Open a zip as a file system, read write or read only
+     *
+     * <p>Note that if it is determined that the zip file is not writable by the
+     * current user, the file system will be read only even if the {@code
+     * readOnly} argument is {@code false}.</p>
+     *
+     * @param path the path to the zip
+     * @param readOnly whether the zip should be opened read only
+     * @return the filesystem
+     * @throws FileSystemNotFoundException zip does not exist
+     * @throws IOException failed to open the path as a zip filesystem
+     */
     @Nonnull
-    public static FileSystem openZip(final Path path, final boolean isReadOnly)
+    @WillNotClose
+    public static FileSystem openZip(final Path path, final boolean readOnly)
         throws IOException
     {
-        Objects.requireNonNull(path);
+        final URI uri = URI.create("jar:" + path.toUri());
 
-        final URI zipURI = URI.create("jar:" + path.toUri());
+        final FileSystem fs = FileSystems.newFileSystem(uri, ZIP_NOCREATE);
 
-        return FileSystems.newFileSystem(zipURI, ZIP_ENV);
+        return readOnly ? new ReadOnlyFileSystem(fs) : fs;
     }
 
+    /**
+     * Create a new zip file as a filesystem
+     *
+     * @param path the path to the zip to create
+     * @return a filesystem
+     * @throws FileAlreadyExistsException zip file already exists
+     * @throws IOException other errors
+     */
+    @Nonnull
+    @WillNotClose
+    public static FileSystem createZip(final Path path)
+        throws IOException
+    {
+        if (Files.exists(path))
+            throw new FileAlreadyExistsException(path.toString());
+
+        final URI uri = URI.create("jar:" + path.toUri());
+
+        return FileSystems.newFileSystem(uri, ZIP_CREATE);
+    }
+
+    /**
+     * Return a read only version of a filesystem, if necessary
+     *
+     * <p>If the filesystem given as an argument is already read only (ie,
+     * {@link FileSystem#isReadOnly()} returns true), this
+     * method will return the filesystem itself.</p>
+     *
+     * @param fs the filesystem
+     * @return a read only filesystem
+     */
+    @Nonnull
+    @WillNotClose
+    public static FileSystem readOnly(final FileSystem fs)
+    {
+        Objects.requireNonNull(fs);
+
+        return fs.isReadOnly() ? fs : new ReadOnlyFileSystem(fs);
+    }
 }
--- src/test/java/com/github/fge/filesystem/MoreFileSystemsTest.java
+++ src/test/java/com/github/fge/filesystem/MoreFileSystemsTest.java
@@ -1,39 +1,121 @@
 package com.github.fge.filesystem;
 
 
+import com.github.fge.filesystem.readonly.ReadOnlyFileSystem;
 import com.github.marschall.memoryfilesystem.MemoryFileSystemBuilder;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.shouldHaveThrown;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public final class MoreFileSystemsTest {
 
     private FileSystem fs;
-    private Path existingZip;
-    private Path nonExistingZip;
+    private Path existing;
+    private Path nonExisting;
 
     @BeforeMethod
     public void initFs()
         throws IOException
     {
         fs = MemoryFileSystemBuilder.newLinux().build("MoreFileSystemsTest");
-        nonExistingZip = fs.getPath("/nonExisting.zip");
-        existingZip = fs.getPath("/existing.zip");
+        nonExisting = fs.getPath("/nonExisting.zip");
+        existing = fs.getPath("/existing.zip");
 
-        final URI uri = URI.create("jar:" + existingZip.toUri());
+        final URI uri = URI.create("jar:" + existing.toUri());
         final Map<String, ?> env = Collections.singletonMap("create", "true");
 
         try (
             final FileSystem tmp = FileSystems.newFileSystem(uri, env);
         ) {
         }
+    }
+
+    @Test
+    public void openZipReadWriteTest()
+        throws IOException
+    {
+        try (
+            final FileSystem fs = MoreFileSystems.openZip(existing, false);
+        ) {
+            assertThat(fs).isNotInstanceOf(ReadOnlyFileSystem.class);
+        }
+    }
+
+    @Test
+    public void openZipReadOnlyTest()
+        throws IOException
+    {
+        try (
+            final FileSystem fs = MoreFileSystems.openZip(existing, true);
+        ) {
+            assertThat(fs).isInstanceOf(ReadOnlyFileSystem.class);
+        }
+    }
+
+    @Test
+    public void createZipNonExistingTest()
+        throws IOException
+    {
+        try (
+            final FileSystem fs = MoreFileSystems.createZip(nonExisting);
+        ) {
+            // nothing
+        }
+
+        assertThat(nonExisting).exists();
+    }
+
+    @Test
+    public void createZipAlreadyExistsTest()
+        throws IOException
+    {
+        try {
+            MoreFileSystems.createZip(existing);
+            shouldHaveThrown(FileAlreadyExistsException.class);
+        } catch (FileAlreadyExistsException ignored) {
+        }
+    }
+
+    @SuppressWarnings("AutoBoxing")
+    @Test
+    public void readOnlyFromReadWriteFsTest()
+    {
+        final FileSystem fs2 = mock(FileSystem.class);
+
+        // This is the default but let's be explicit
+        when(fs2.isReadOnly()).thenReturn(false);
+
+        final FileSystem rofs = MoreFileSystems.readOnly(fs2);
+
+        assertThat(rofs).isNotSameAs(fs2)
+            .isInstanceOf(ReadOnlyFileSystem.class);
+    }
+
+    @SuppressWarnings("AutoBoxing")
+    @Test
+    public void readOnlyFromReadOnlyFsTest()
+    {
+        final FileSystem fs2 = mock(FileSystem.class);
+
+        when(fs2.isReadOnly()).thenReturn(true);
+
+        final FileSystem rofs = MoreFileSystems.readOnly(fs2);
+
+        assertThat(rofs).isSameAs(fs2);
     }
 
     @AfterMethod
